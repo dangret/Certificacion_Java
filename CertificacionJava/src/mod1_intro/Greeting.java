b38cb43b880db808cdaf4efff9c4f45f0f9c945f
daniel-negrete <daniel.negrete@sinneg.com>
2014-10-25 11:16:01 -0600

    wdawd

---
+++ CertificacionJava/src/mod1_intro/Greeting.java
@@ -0,0 +1,7 @@
+package mod1_intro;
+
+public class Greeting {
+	public void greet(){
+		System.out.println("hola mundo");
+	}
+}
---
+++ CertificacionJava/src/mod1_intro/TestGreeting.java
@@ -0,0 +1,10 @@
+package mod1_intro;
+
+public class TestGreeting {
+
+	public static void main(String[] args) {
+		Greeting greeting = new Greeting();
+		greeting.greet();
+	}
+
+}
